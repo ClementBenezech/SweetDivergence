ab9b9432ab7966463cc3bc190ce248aa965f90cc
Clement Benezech <clement.benezech@gmail.com>
2021-05-08 13:37:40 +0200

    added dynamic sizing for content depending on the menu being visible or not

--- public/script/scripts.js
+++ public/script/scripts.js
@@ -10,6 +10,7 @@ document.getElementById("nav-bar__item-hide").addEventListener("click", hideNavB
 
 function navigateToListen() {
     document.getElementById(currentContent).classList.remove("enter-left");
+    document.getElementById(currentContent).classList.remove ("page-content--normal");
     document.getElementById(currentContent).classList.add("exit-right");
     document.getElementById("listen-content").classList.add("enter-left");
     document.getElementById("listen-content").classList.remove("exit-right");
@@ -19,6 +20,7 @@ function navigateToListen() {
 function navigateToConcert() {
     console.log(currentContent);
     document.getElementById(currentContent).classList.remove("enter-left");
+    document.getElementById(currentContent).classList.remove ("page-content--normal");
     document.getElementById(currentContent).classList.add("exit-right");
     document.getElementById("concert-content").classList.add("enter-left");
     document.getElementById("concert-content").classList.remove("exit-right");
@@ -27,6 +29,7 @@ function navigateToConcert() {
 
 function navigateToBand() {
     document.getElementById(currentContent).classList.remove("enter-left");
+    document.getElementById(currentContent).classList.remove ("page-content--normal");
     document.getElementById(currentContent).classList.add("exit-right");
     document.getElementById("band-content").classList.add("enter-left");
     document.getElementById("band-content").classList.remove("exit-right");
@@ -35,6 +38,7 @@ function navigateToBand() {
 
 function navigateToPictures() {
     document.getElementById(currentContent).classList.remove("enter-left");
+    document.getElementById(currentContent).classList.remove ("page-content--normal");
     document.getElementById(currentContent).classList.add("exit-right");
     document.getElementById("pictures-content").classList.add("enter-left");
     document.getElementById("pictures-content").classList.remove("exit-right");
@@ -43,8 +47,10 @@ function navigateToPictures() {
 
 function navigateToHome() {
     document.getElementById(currentContent).classList.remove("enter-left");
+    document.getElementById(currentContent).classList.remove ("page-content--normal");
     document.getElementById(currentContent).classList.add("exit-right");
     document.getElementById("page-content").classList.add("enter-left")
+    document.getElementById("pictures-content").classList.remove("exit-right");
     currentContent = document.getElementById("page-content").id
 }
 
@@ -53,18 +59,27 @@ function hideNavBar () {
     document.getElementById("nav-bar").classList.remove ("nav-bar--initial");
     document.getElementById("nav-bar").classList.remove ("nav-bar--visible");
     document.getElementById("nav-bar__item-hide").classList.add("nav-bar__item--upside-down")
+
     document.getElementById("nav-bar__item-hide").removeEventListener("click", hideNavBar, true);
     document.getElementById("nav-bar__item-hide").addEventListener("click", showNavBar, true);
+
     document.getElementById(currentContent).classList.add("page-content--extended");
+    document.getElementById(currentContent).classList.remove ("enter-left");
+    document.getElementById(currentContent).classList.remove ("page-content--normal");
+    
 
 }
 function showNavBar () {
     document.getElementById("nav-bar").classList.remove ("nav-bar--hidden");
     document.getElementById("nav-bar").classList.add("nav-bar--visible")
     document.getElementById("nav-bar__item-hide").classList.remove("nav-bar__item--upside-down")
+
     document.getElementById("nav-bar__item-hide").removeEventListener("click", showNavBar, true);
     document.getElementById("nav-bar__item-hide").addEventListener("click", hideNavBar, true);
+
+    document.getElementById(currentContent).classList.add ("page-content--normal");
     document.getElementById(currentContent).classList.remove("page-content--extended");
+    
 
 }
 
