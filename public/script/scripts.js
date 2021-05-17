716a7786c6a3389c8fb93a130b5c04fbe4a5be87
Clement Benezech <clementbenezech@gmail.com>
2021-05-17 12:35:27 +0200

    Major refactor of the JS code. Created a navigatetToPage function to handle content switch, created a toggleNavBar function to handle the well. navbar :)

--- public/script/scripts.js
+++ public/script/scripts.js
@@ -1,95 +1,70 @@
+/*Initialisation des variables d'affichage: page actuelle et largeur actuelle*/
 var currentContent = "home-content";
+var currentWindowWidth = "--normal";
+
+/*Creation des EventListeners pour les liens du menu principal*/
+document.getElementById("nav-bar__image").addEventListener("click", () => {
+    navigateToPage("home");
+})
 
 
-document.getElementById("nav-bar__item-1").addEventListener("click", navigateToListen, true);
-document.getElementById("nav-bar__item-2").addEventListener("click", navigateToConcert, true);
-document.getElementById("nav-bar__item-3").addEventListener("click", navigateToBand, true);
-document.getElementById("nav-bar__item-4").addEventListener("click", navigateToPictures, true);
-document.getElementById("nav-bar__image").addEventListener("click", navigateToHome, true);
-document.getElementById("nav-bar__item-hide").addEventListener("click", hideNavBar, true);
+document.getElementById("nav-bar__item-1").addEventListener("click", () => {
+    navigateToPage("listen");
+})
 
-function navigateToListen() {
+document.getElementById("nav-bar__item-2").addEventListener("click", () => {
+    navigateToPage("concert");
+})
+
+document.getElementById("nav-bar__item-3").addEventListener("click", () => {
+    navigateToPage("band");
+})
+
+document.getElementById("nav-bar__item-4").addEventListener("click", () => {
+    navigateToPage("pictures");
+})
+
+/*Creation d'un eventListener pour gérer l'affichage ou non de la navbar sur mobile*/
+
+document.getElementById("nav-bar__item-hide").addEventListener("click", toggleNavBar, true);
+
+/*Fonction qui gère le 'désaffichage" de la page en cours et l'affichage de la page demandée*/
+function navigateToPage($page) {
     document.getElementById(currentContent).classList.remove("enter-left");
     document.getElementById(currentContent).classList.remove ("page-content--normal");
+    document.getElementById(currentContent).classList.remove ("page-content--extended");
     document.getElementById(currentContent).classList.add("exit-right");
-    document.getElementById("listen-content").classList.add("enter-left");
-    document.getElementById("listen-content").classList.remove("exit-right");
-    currentContent = document.getElementById("listen-content").id;
+    document.getElementById($page+"-content").classList.add("enter-left");
+    document.getElementById($page+"-content").classList.remove("exit-right");
+    currentContent = document.getElementById($page+"-content").id;
 }
 
-function navigateToConcert() {
-    console.log(currentContent);
-    document.getElementById(currentContent).classList.remove("enter-left");
-    document.getElementById(currentContent).classList.remove ("page-content--normal");
-    document.getElementById(currentContent).classList.add("exit-right");
-    document.getElementById("concert-content").classList.add("enter-left");
-    document.getElementById("concert-content").classList.remove("exit-right");
-    currentContent = document.getElementById("concert-content").id;
-}
+/*Fonction qui gère minimize/expand le manu principal*/
 
-function navigateToBand() {
-    document.getElementById(currentContent).classList.remove("enter-left");
-    document.getElementById(currentContent).classList.remove ("page-content--normal");
-    document.getElementById(currentContent).classList.add("exit-right");
-    document.getElementById("band-content").classList.add("enter-left");
-    document.getElementById("band-content").classList.remove("exit-right");
-    currentContent = document.getElementById("band-content").id
-}
-
-function navigateToPictures() {
-    document.getElementById(currentContent).classList.remove("enter-left");
-    document.getElementById(currentContent).classList.remove ("page-content--normal");
-    document.getElementById(currentContent).classList.add("exit-right");
-    document.getElementById("pictures-content").classList.add("enter-left");
-    document.getElementById("pictures-content").classList.remove("exit-right");
-    currentContent = document.getElementById("pictures-content").id
-}
-
-function navigateToHome() {
-    document.getElementById(currentContent).classList.remove("enter-left");
-    document.getElementById(currentContent).classList.remove ("page-content--normal");
-    document.getElementById(currentContent).classList.add("exit-right");
-    document.getElementById("home-content").classList.add("enter-left")
-    document.getElementById("home-content").classList.remove("exit-right");
-    currentContent = document.getElementById("home-content").id
-}
-
-function hideNavBar () {
-    document.getElementById("nav-bar").classList.add("nav-bar--hidden");
-    document.getElementById("nav-bar").classList.remove ("nav-bar--initial");
-    document.getElementById("nav-bar").classList.remove ("nav-bar--visible");
-    document.getElementById("nav-bar__item-hide").classList.add("nav-bar__item--upside-down")
-
-    document.getElementById("nav-bar__item-hide").removeEventListener("click", hideNavBar, true);
-    document.getElementById("nav-bar__item-hide").addEventListener("click", showNavBar, true);
-
-    document.getElementById(currentContent).classList.add("page-content--extended");
-    document.getElementById(currentContent).classList.remove ("enter-left");
-    document.getElementById(currentContent).classList.remove ("page-content--normal");
-    
-
-}
-function showNavBar () {
-    document.getElementById("nav-bar").classList.remove ("nav-bar--hidden");
-    document.getElementById("nav-bar").classList.add("nav-bar--visible")
-    document.getElementById("nav-bar__item-hide").classList.remove("nav-bar__item--upside-down")
-
-    document.getElementById("nav-bar__item-hide").removeEventListener("click", showNavBar, true);
-    document.getElementById("nav-bar__item-hide").addEventListener("click", hideNavBar, true);
-
-    document.getElementById(currentContent).classList.add ("page-content--normal");
-    document.getElementById(currentContent).classList.remove("page-content--extended");
-    
-
+function toggleNavBar () {
+    switch(currentWindowWidth) {
+    case "--normal":
+        document.getElementById("nav-bar").classList.add("nav-bar--hidden");
+        document.getElementById("nav-bar").classList.remove ("nav-bar--initial");
+        document.getElementById("nav-bar").classList.remove ("nav-bar--visible");
+        document.getElementById("nav-bar__item-hide").classList.add("nav-bar__item--upside-down")
+        document.getElementById(currentContent).classList.add("page-content--extended");
+        document.getElementById(currentContent).classList.remove ("enter-left");
+        document.getElementById(currentContent).classList.remove ("page-content--normal");
+        currentWindowWidth = "--extended";
+        break;
+    case"--extended":
+        document.getElementById("nav-bar").classList.remove ("nav-bar--hidden");
+        document.getElementById("nav-bar").classList.add("nav-bar--visible")
+        document.getElementById("nav-bar__item-hide").classList.remove("nav-bar__item--upside-down")
+        document.getElementById(currentContent).classList.add ("page-content--normal");
+        document.getElementById(currentContent).classList.remove("page-content--extended");
+        currentWindowWidth = "--normal";
+        break;
+    }
 }
 
 
-
-
-/********************************************************************************************* */
-function initializePage(){
-    document.getElementById("nav-bar__item-1").addEventListener("click", navigate());
-}
 
 
 
